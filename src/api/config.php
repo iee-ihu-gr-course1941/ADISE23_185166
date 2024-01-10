220cc1cab14de3192413e35a3362a7e492600921
Dean Joanidhi <deanj.development@gmail.com>
2024-01-10 15:47:57 +0200

    added an echo command to config

--- src/api/config.php
+++ src/api/config.php
@@ -14,6 +14,8 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 /* Connection Error Handling */
 if ($conn->connect_error){
     die("Connection failed: ". $conn->connect_error);
+} else {
+    echo "Connection was successful!";
 }
 
 ?>
